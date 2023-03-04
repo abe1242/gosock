ff397926b26ddfea420c7844f4a595a18ec1d31e
Abe <abmdn1242@gmail.com>
2023-03-04 13:33:05 +0530

    Completed the clip functionality

--- entity/clip.go
+++ entity/clip.go
@@ -17,3 +17,15 @@ func clipGet() io.ReadCloser {
     }
     return stdout
 }
+
+func clipSet() io.WriteCloser {
+    cmd := exec.Command("termux-clipboard-set")
+    stdin, err := cmd.StdinPipe()
+    if err != nil {
+        return nil
+    }
+    if err = cmd.Start(); err != nil {
+        return nil
+    }
+    return stdin
+}

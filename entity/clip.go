0470941f9083b586efd572855387010ecb12326d
Abe <abmdn1242@gmail.com>
2023-03-04 14:57:12 +0530

    Added xclip supports(to be tested)

--- entity/clip.go
+++ entity/clip.go
@@ -3,10 +3,21 @@ package entity
 import (
 	"os/exec"
 	"io"
+	"os"
+	"runtime"
+	"fmt"
 )
 
 func clipGet() io.ReadCloser {
-    cmd := exec.Command("termux-clipboard-get")
+    var cmd *exec.Cmd
+    if runtime.GOOS == "android" {
+        cmd = exec.Command("termux-clipboard-get")
+    } else if runtime.GOOS == "linux" {
+        cmd = exec.Command("xclip -sel clip -o")
+    } else {
+        fmt.Fprintln(os.Stderr, "Your OS doesn't support this option (--clipboard)")
+        os.Exit(1);
+    }
     stdout, err := cmd.StdoutPipe()
     cmd.Stderr = cmd.Stdout
     if err != nil {
@@ -19,7 +30,15 @@ func clipGet() io.ReadCloser {
 }
 
 func clipSet() io.WriteCloser {
-    cmd := exec.Command("termux-clipboard-set")
+    var cmd *exec.Cmd
+    if runtime.GOOS == "android" {
+        cmd = exec.Command("termux-clipboard-set")
+    } else if runtime.GOOS == "linux" {
+        cmd = exec.Command("xclip -sel clip")
+    } else {
+        fmt.Fprintln(os.Stderr, "Your OS doesn't support this option (--clipboard)")
+        os.Exit(1);
+    }
     stdin, err := cmd.StdinPipe()
     if err != nil {
         return nil
